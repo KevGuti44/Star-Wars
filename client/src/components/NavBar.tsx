fca8088fd74146e2f66e439515c69e22fa64c4d9
Kevin <kg71875@gmail.com>
2025-04-02 11:09:31 +0200

    CSS nav bar ok

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import "../styles/navbar.css";
 export default function NavBar() {
   return (
     <>
-      <nav>
+      <nav className="nav_container">
         <Link to="/">Accueil</Link>
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/jedi">Jedi</Link>
